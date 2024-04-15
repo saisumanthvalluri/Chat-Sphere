11f53a6dad4cd243957d2b2d86440ef03f218f5c
saisumanth valluri <saisumanth.valluri@infinitylearn.com>
2024-04-15 14:03:11 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+const App = () => {
+  return (
+    <div className=''>
+      <h1>Hello World!</h1>
+      <p>This is my first React app!</p>
+      <h1>Hello World!</h1>
+      <p>This is my first React app!</p>
+      <h1>Hello World!</h1>
+      <p>This is my first React app!</p>
+      <h1>Hello World!</h1>
+      <p>This is my first React app!</p>
+
+      <h1>Hello World!</h1>
+      <p>This is my first React app!</p>
+      <h1>Hello World!</h1>
+      <p>This is my first React app!</p><h1>Hello World!</h1>
+      <p>This is my first React app!</p>
+      <h1>Hello World!</h1>
+      <p>This is my first React app!</p>
+      <var><h1>Hello World!</h1>
+      <p>This is my first React app!</p>
+      <h1>Hello World!</h1>
+      <p>This is my first React app!</p></var>
+      <h1>Hello World!</h1>
+      <p>This is my first React app!</p>
+      <h1>Hello World!</h1>
+      <p>This is my first React app!</p>
+      <h1>Hello World!</h1>
+      <p>This is my first React app!</p>
+    </div>
+  )
+}
+
+export default App
